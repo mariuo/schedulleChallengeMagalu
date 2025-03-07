2efa02114b9e9509ccce22ffd5e84ee0b9f0494a
Mario Camelo <mariuo@gmail.com>
2025-03-07 18:03:47 -0500

    remove: Mappers Structs

--- src/main/java/com/camel/scheduleNotification/controllers/CommunicationSchedule.java
+++
@@ -1,9 +0,0 @@
-package com.camel.scheduleNotification.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/schedule")
-public class CommunicationSchedule {
-}
