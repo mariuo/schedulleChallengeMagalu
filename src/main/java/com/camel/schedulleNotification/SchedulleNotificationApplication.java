4cb564b84fbe271c28ea3065057328200499bacd
Mario Camelo <mariuo@gmail.com>
2025-03-07 15:22:39 -0500

    feat: Project created

---
+++ src/main/java/com/camel/schedulleNotification/SchedulleNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.camel.schedulleNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulleNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulleNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camel/schedulleNotification/SchedulleNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.schedulleNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulleNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
