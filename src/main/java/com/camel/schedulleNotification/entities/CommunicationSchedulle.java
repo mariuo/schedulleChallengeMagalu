1bf340e1de7db593604f4df5d137f0111f48abd5
Mario Camelo <mariuo@gmail.com>
2025-03-07 16:02:04 -0500

    feat: Added DataJPA to entity CommunicationSchedulle

--- src/main/java/com/camel/schedulleNotification/entities/CommunicationSchedulle.java
+++ src/main/java/com/camel/schedulleNotification/entities/CommunicationSchedulle.java
@@ -1,17 +1,41 @@
 package com.camel.schedulleNotification.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "communication_schedules")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommunicationSchedulle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(nullable = false)
     private LocalDateTime date;
+
+    @Column(nullable = false)
     private String message;
+
+    @Column(nullable = false)
     private String recipient;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private ScheduleStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private ScheduleType type;
 
+    @Column(nullable = false)
     private LocalDateTime creadtedAt;
     private LocalDateTime updatedAt;
 
