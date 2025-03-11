9c5eec7730bd800aca16f13c7f1be97105c514f2
Mario Camelo <mariuo@gmail.com>
2025-03-10 21:33:51 -0400

    feat: ControllerAdvice exception.

--- src/main/java/com/camel/scheduleNotification/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/camel/scheduleNotification/exceptions/ControllerExceptionHandler.java
@@ -1,8 +1,25 @@
 package com.camel.scheduleNotification.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+        ErrorMessage msg = new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+     return new ResponseEntity<ErrorMessage>(msg, HttpStatus.NOT_FOUND);
+    }
 
 }
